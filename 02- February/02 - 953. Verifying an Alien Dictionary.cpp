84027b9e3edda9c04cc20097405de696a9b2796c
Gerges Hany <105644935+GergesHany@users.noreply.github.com>
2023-04-02 06:34:18 +0200

    Update 02 - 953. Verifying an Alien Dictionary.cpp

--- 02- February/02 - 953. Verifying an Alien Dictionary.cpp
+++ 02- February/02 - 953. Verifying an Alien Dictionary.cpp
@@ -3,26 +3,27 @@ public:
     bool isAlienSorted(vector<string>& words, string order) {
         int j = 0;
         map < char, int > mp;
-        for(auto &it : order) mp[it] = j++;
+        for(auto &it : order) mp[it] = j++; // map each char to its index in the order string
 
-        for(int i = 1; i < words.size(); i++ ){
+        for(int i = 1; i < words.size(); i++){
 
-            bool flag = false ;
-            string first = words[i - 1];
-            string second = words[i];
+            bool flag = false; // flag to check if the first string is smaller than the second string  
+            string first = words[i - 1]; // first string
+            string second = words[i]; // second string
 
-            int minLen = min(first.size(), second.size());
+            int minLen = min(first.size(), second.size()); // get the minimum length of the two strings
 
-            for(int j = 0 ; j < minLen ; j++){
+            for(int j = 0; j < minLen; j++){
+                // if the first string is smaller than the second string
                 if(mp[first[j]] < mp[second[j]]) {
-                    flag = true ;
-                    break;
+                    flag = true; // set the flag to true 
+                    break; // break the loop
                 }
-                if(mp[first[j]] > mp[second[j]]) return false;
-            }
-            if(!flag && first.size() > second.size()) return false;
-        }
 
-        return true;
+                if(mp[first[j]] > mp[second[j]]) return false; // if the first string is greater than the second string return false
+            }
+            if(!flag && first.size() > second.size()) return false; // if the flag is false and the first string is greater than the second string return false
+        }
+        return true; // return true if all the strings are sorted
     }
 };
