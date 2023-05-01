d86da0263efbecc50d39d34dbe5b21c67fe710ad
Gerges Hany <gergeshany179@gmail.com>
2023-05-01 09:10:50 +0300

    add solution 1

---
+++ 05-May/01 - 1491. Average Salary Excluding the Minimum and Maximum Salary.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double average(vector<int>& salary) {
+
+       long double sum = 0; // sum of all salaries except min and max
+
+       int mn = *min_element(salary.begin(), salary.end()); // min salary
+       int mx = *max_element(salary.begin(), salary.end()); // max salary
+       
+       int sz = 0; // size of salary vector except min and max
+       for (auto & i: salary)
+        // if salary is not min and max then add it to sum and increase size
+         if (i != mn && i != mx) 
+            sum += i, sz++;   
+        // return average    
+       return sum / double(sz);      
+    }
+};
